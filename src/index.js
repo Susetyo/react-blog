8784c7948a74bc8b40c7f4f05a694cd2833a73b6
Susetya Mulia Pambudi <susetyamulia@gmail.com>
2020-01-26 00:01:49 +0700

    my react blog

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Navbar from './components/NavBar';
+import Welcome from './components/Welcome';
+import Footer from './components/Footer';
+import CreateArticle from './components/CreateArticle';
+import Login from './components/Login';
+import SingleArticle from './components/SingleArticle';
+import SignUp from './components/SignUp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Home = () => <h1>This is Home</h1>
+const About = () => <h1>This is About</h1>
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Navbar />
+            <Route exact path="/" component={Welcome}></Route>
+            <Route path="/home" component={Home}></Route>
+            <Route path="/about" component={About}></Route>
+            <Route path="/articles/create" component={CreateArticle}></Route>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/signUp" component={SignUp}></Route>
+            <Route path="/article/:slug" component={SingleArticle}></Route>
+            <Footer />
+        </div>
+    </BrowserRouter>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
