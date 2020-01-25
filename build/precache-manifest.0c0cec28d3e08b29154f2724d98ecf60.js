5356ffd2bf536154a5ad85b59d50962befb717c2
Susetya Mulia Pambudi <susetyamulia@gmail.com>
2020-01-26 00:14:04 +0700

    ignore build production

---
+++ build/precache-manifest.0c0cec28d3e08b29154f2724d98ecf60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81aaefa331d381d6881605a0d773bb40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa7092563505a30b6c2a",
+    "url": "/static/js/2.ce67a359.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ce67a359.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d50f511bf2d8e5416a9d",
+    "url": "/static/js/main.b10592da.chunk.js"
+  },
+  {
+    "revision": "91de3d6c4e87c90b7d90",
+    "url": "/static/js/runtime-main.7ffca68f.js"
+  }
+]);
